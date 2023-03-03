3ddde50908cfee75f14eb38266247cf0ba005182
胡啸 <603316223hx@gmail.com>
2023-03-03 10:09:12 +0800

    gpt4

--- gpt/app.py
+++ gpt/app.py
@@ -5,6 +5,8 @@ openai.api_key = "xUtfY5DiO7V2CpWCFrUqT3BlbkFJs2H1Ap4E9NRQQc7LQ40I"
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
@@ -12,7 +14,7 @@ def home():
 @app.route('/generate_text', methods=['POST'])
 def generate_text():
     prompt = request.form.get('prompt')
-    print("Received prompt:", prompt)
+    logging.debug("Received prompt: %s", prompt)
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -22,7 +24,7 @@ def generate_text():
         temperature=0.5,
     )
     generated_text = response.choices[0].text
-    print("Generated text:", generated_text)
+    logging.debug("Generated text: %s", generated_text)
     return jsonify(generated_text)
 
 if __name__ == '__main__':
