bb997de84d51a3928e7d8e3958176fc3804ea5f7
胡啸 <603316223hx@gmail.com>
2023-03-03 11:30:10 +0800

    gpt3

--- gpt/app.py
+++ gpt/app.py
@@ -1,6 +1,6 @@
 import os
 import openai
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,15 @@ openai.api_key = os.getenv("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"
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/chat')
+def chat():
+    message = request.args.get('message')
+
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+        prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: " + message + "\nAI:",
         temperature=0.9,
         max_tokens=150,
         top_p=1,
@@ -18,8 +24,8 @@ def home():
         presence_penalty=0.6,
         stop=[" Human:", " AI:"]
     )
-    response_text = response.choices[0].text
-    return render_template('index.html', response_text=response_text)
+    response_text = response.choices[0].text.strip()
+    return response_text
 
 if __name__ == '__main__':
     app.run(debug=True)
